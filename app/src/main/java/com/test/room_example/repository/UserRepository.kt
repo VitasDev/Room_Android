96a055398bec496c4230843419dbd004a1eb9f65
VitasDev <vitas.kolesnichenko100@gmail.com>
2022-10-10 01:03:22 +0300

    First commit

---
+++ app/src/main/java/com/test/room_example/repository/UserRepository.kt
@@ -0,0 +1,26 @@
+package com.test.room_example.repository
+
+import androidx.lifecycle.LiveData
+import com.test.room_example.data.UserDao
+import com.test.room_example.model.User
+
+class UserRepository(private val userDao: UserDao) {
+
+    val readAllData: LiveData<List<User>> = userDao.readAllData()
+
+    suspend fun addUser(user: User) {
+        userDao.addUser(user)
+    }
+
+    suspend fun updateUser(user: User) {
+        userDao.updateUser(user)
+    }
+
+    suspend fun deleteUser(user: User) {
+        userDao.deleteUser(user)
+    }
+
+    suspend fun deleteAllUsers() {
+        userDao.deleteAllUsers()
+    }
+}
